15df930017b37d2a155eb03f4b18df49c9a8039d
Mateus Matos <matcod2@gmail.com>
2021-10-13 08:18:24 -0300

    Pagina de login

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    pallete: {
+        primary: {
+            main: '#000000'
+        }
+    },
+    spacing: 8,
+});
+
+export default theme;
